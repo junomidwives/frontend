cd1c23c61c78c8354877c79df3fd4a6177d87b3b
Stuart Sewell <stuart@preoptima.com>
2025-03-13 17:11:33 +0000

    initialize Nuxt.js project with Vuetify, TypeScript, and essential files

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-layout>
+      <app-bar />
+
+      <v-main>
+        <v-img
+          src="/img/download.jpg"
+          width="100%"
+          max-height="480"
+          cover
+          class="hero-image"
+        >
+        </v-img>
+      </v-main>
+    </v-layout>
+  </v-app>
+</template>
+
+<style>
+.hero-image {
+  filter: brightness(0.75);
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,45 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+import { createResolver } from "@nuxt/kit";
+
+const { resolve } = createResolver(import.meta.url);
+
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+    "@nuxtjs/google-fonts",
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+  googleFonts: {
+    families: {
+      "DM+Sans": [400, 500, 700],
+      "Material+Icons": true,
+    },
+  },
+  hooks: {
+    "vite:extendConfig": (config) => {
+      // @ts-expect-error
+      config.plugins.push(
+        vuetify({
+          styles: { configFile: resolve("./settings.scss") },
+        })
+      );
+    },
+  },
+});
