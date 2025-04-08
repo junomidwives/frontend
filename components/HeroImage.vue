f207ce2fd98603adca3af853c08ce7b530ad201c
Stuart Sewell <stuart@preoptima.com>
2025-04-08 14:33:53 +0100

    Wrap HeroImage component in client-only tag for improved SSR handling

--- components/HeroImage.vue
+++ components/HeroImage.vue
@@ -1,29 +1,31 @@
 <template>
-  <v-img
-    v-if="showHero"
-    src="/img/splash.jpg"
-    cover
-    :max-height="400"
-    class="hero-image"
-  >
-    <v-sheet
-      width="100%"
-      height="100%"
-      color="transparent"
-      class="d-flex align-end pa-4 pa-md-16 text-h5 text-sm-h3 text-md-h2 text-white"
+  <client-only>
+    <v-img
+      v-if="showHero"
+      src="/img/splash.jpg"
+      cover
+      :max-height="400"
+      class="hero-image"
     >
-      <div v-if="heroText && heroText !== 'Home'">
-        <p class="font-weight-bold">{{ heroText }}</p>
-      </div>
-      <div v-else>
-        <p class="font-weight-bold">Natasha & Sophie</p>
-        <p class="font-weight-thin">
-          Independent midwives<br />
-          in Kent
-        </p>
-      </div>
-    </v-sheet>
-  </v-img>
+      <v-sheet
+        width="100%"
+        height="100%"
+        color="transparent"
+        class="d-flex align-end pa-4 pa-md-16 text-h5 text-sm-h3 text-md-h2 text-white"
+      >
+        <div v-if="heroText && heroText !== 'Home'">
+          <p class="font-weight-bold">{{ heroText }}</p>
+        </div>
+        <div v-else>
+          <p class="font-weight-bold">Natasha & Sophie</p>
+          <p class="font-weight-thin">
+            Independent midwives<br />
+            in Kent
+          </p>
+        </div>
+      </v-sheet>
+    </v-img>
+  </client-only>
 </template>
 
 <script setup lang="ts">
