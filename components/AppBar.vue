1f836bd2592ec10da3bc31b738662ab23944328c
Stuart Sewell <stuart@preoptima.com>
2025-04-10 15:28:30 +0100

    Refactor AppBar and blog slug components to remove unnecessary client-only wrappers and streamline template structure

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -1,111 +1,104 @@
 <template>
-  <client-only>
-    <v-app-bar :height="xs ? 60 : 100" class="px-md-5" color="teal-lighten-5">
-      <v-container fluid>
-        <v-row>
-          <v-col
-            sm="4"
-            class="d-flex align-center justify-space-between text-grey-darken-4"
-          >
-            <h1 class="text-h5 text-md-h4 font-weight-bold">
-              <nuxt-link
-                to="/"
-                class="text-decoration-none text-surface-variant"
-              >
-                juno midwives
-              </nuxt-link>
-            </h1>
-            <v-btn
-              icon="mdi-menu"
-              @click="showMenu = !showMenu"
-              class="d-sm-none"
-            />
-          </v-col>
+  <v-app-bar :height="xs ? 60 : 100" class="px-md-5" color="teal-lighten-5">
+    <v-container fluid>
+      <v-row>
+        <v-col
+          sm="4"
+          class="d-flex align-center justify-space-between text-grey-darken-4"
+        >
+          <h1 class="text-h5 text-md-h4 font-weight-bold">
+            <nuxt-link to="/" class="text-decoration-none text-surface-variant">
+              juno midwives
+            </nuxt-link>
+          </h1>
+          <v-btn
+            icon="mdi-menu"
+            @click="showMenu = !showMenu"
+            class="d-sm-none"
+          />
+        </v-col>
 
-          <v-col
-            class="justify-end d-none d-sm-flex flex-wrap align-center ga-1"
+        <v-col class="justify-end d-none d-sm-flex flex-wrap align-center ga-1">
+          <v-btn
+            nuxt
+            to="/"
+            color="surface-variant"
+            class="text-subtitle-1 px-2 px-md-3"
           >
+            Home
+          </v-btn>
+          <template v-for="item in menuItems" :key="item.title">
+            <v-menu v-if="item.subitems" open-on-hover offset="6">
+              <template #activator="{ props }">
+                <v-btn
+                  v-bind="props"
+                  color="surface-variant"
+                  class="text-subtitle-1 px-2 px-md-3"
+                  append-icon="mdi-chevron-down"
+                >
+                  {{ item.title }}
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item
+                  v-for="subitem in item.subitems"
+                  :key="subitem.title"
+                  density="compact"
+                  nuxt
+                  :to="subitem.link"
+                >
+                  <v-list-item-title
+                    class="text-subtitle-1 text-surface-variant"
+                  >
+                    {{ subitem.title }}
+                  </v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+
             <v-btn
+              v-else
               nuxt
-              to="/"
+              :to="item.link"
               color="surface-variant"
               class="text-subtitle-1 px-2 px-md-3"
             >
-              Home
+              {{ item.title }}
             </v-btn>
-            <template v-for="item in menuItems" :key="item.title">
-              <v-menu v-if="item.subitems" open-on-hover offset="6">
-                <template #activator="{ props }">
-                  <v-btn
-                    v-bind="props"
-                    color="surface-variant"
-                    class="text-subtitle-1 px-2 px-md-3"
-                    append-icon="mdi-chevron-down"
-                  >
-                    {{ item.title }}
-                  </v-btn>
-                </template>
-                <v-list>
-                  <v-list-item
-                    v-for="subitem in item.subitems"
-                    :key="subitem.title"
-                    density="compact"
-                    nuxt
-                    :to="subitem.link"
-                  >
-                    <v-list-item-title
-                      class="text-subtitle-1 text-surface-variant"
-                    >
-                      {{ subitem.title }}
-                    </v-list-item-title>
-                  </v-list-item>
-                </v-list>
-              </v-menu>
+          </template>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app-bar>
 
-              <v-btn
-                v-else
-                nuxt
-                :to="item.link"
-                color="surface-variant"
-                class="text-subtitle-1 px-2 px-md-3"
-              >
-                {{ item.title }}
-              </v-btn>
-            </template>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-app-bar>
-
-    <v-navigation-drawer
-      v-model="showMenu"
-      temporary
-      location="bottom"
-      class="rounded-t-xl"
-    >
-      <v-list>
-        <template v-for="item in menuItems" :key="item.title">
-          <v-list-group v-if="item.subitems" :value="item.title">
-            <template #activator="{ props }">
-              <v-list-item v-bind="props">
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
-              </v-list-item>
-            </template>
-
-            <v-list-item
-              v-for="subitem in item.subitems"
-              :key="subitem.title"
-              :to="subitem.link"
-            >
-              <v-list-item-title>{{ subitem.title }}</v-list-item-title>
+  <v-navigation-drawer
+    v-model="showMenu"
+    temporary
+    location="bottom"
+    class="rounded-t-xl"
+  >
+    <v-list>
+      <template v-for="item in menuItems" :key="item.title">
+        <v-list-group v-if="item.subitems" :value="item.title">
+          <template #activator="{ props }">
+            <v-list-item v-bind="props">
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
             </v-list-item>
-          </v-list-group>
+          </template>
 
-          <v-list-item v-else :to="item.link" :title="item.title" />
-        </template>
-      </v-list>
-    </v-navigation-drawer>
-  </client-only>
+          <v-list-item
+            v-for="subitem in item.subitems"
+            :key="subitem.title"
+            :to="subitem.link"
+          >
+            <v-list-item-title>{{ subitem.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list-group>
+
+        <v-list-item v-else :to="item.link" :title="item.title" />
+      </template>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script setup lang="ts">
--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,67 +1,62 @@
 <template>
-  <client-only>
-    <v-container v-if="blog" max-width="1000" class="mx-auto">
-      <v-img
-        :src="$urlFor(blog.image).size(800, 500).url()"
-        cover
-        width="100%"
-        max-height="500"
-        aspect-ratio="1"
-        rounded="xl"
-        class="mt-10"
-      />
+  <v-container v-if="blog" max-width="1000" class="mx-auto">
+    <v-img
+      :src="$urlFor(blog.image).size(800, 500).url()"
+      cover
+      width="100%"
+      max-height="500"
+      aspect-ratio="1"
+      rounded="xl"
+      class="mt-10"
+    />
 
-      <h2 class="text-h4 font-weight-bold mt-8 mb-2">{{ blog.title }}</h2>
-      <p class="text-body-2 mb-8">{{ formattedDate }}</p>
+    <h2 class="text-h4 font-weight-bold mt-8 mb-2">{{ blog.title }}</h2>
+    <p class="text-body-2 mb-8">{{ formattedDate }}</p>
 
-      <div class="text-body-1">
-        <SanityContent
-          :blocks="blog.content?.body"
-          :serializers="serializers"
-        />
-      </div>
+    <div class="text-body-1">
+      <SanityContent :blocks="blog.content?.body" :serializers="serializers" />
+    </div>
 
-      <v-divider class="mt-16 mb-4" />
+    <v-divider class="mt-16 mb-4" />
 
-      <v-row>
-        <v-col>
-          <v-btn
-            v-if="previousBlog"
-            nuxt
-            :to="`/blog/${previousBlog.slug.current}`"
-            prepend-icon="mdi-arrow-left"
-            variant="text"
-            size="x-large"
-          >
-            <div class="d-flex flex-column mt-n1">
-              <span class="text-button"> Previous </span>
-              <span class="text-body-1 mt-n2">
-                {{ previousBlog.title }}
-              </span>
-            </div>
-          </v-btn>
-        </v-col>
+    <v-row>
+      <v-col>
+        <v-btn
+          v-if="previousBlog"
+          nuxt
+          :to="`/blog/${previousBlog.slug.current}`"
+          prepend-icon="mdi-arrow-left"
+          variant="text"
+          size="x-large"
+        >
+          <div class="d-flex flex-column mt-n1">
+            <span class="text-button"> Previous </span>
+            <span class="text-body-1 mt-n2">
+              {{ previousBlog.title }}
+            </span>
+          </div>
+        </v-btn>
+      </v-col>
 
-        <v-col class="text-right">
-          <v-btn
-            v-if="nextBlog"
-            nuxt
-            :to="`/blog/${nextBlog.slug.current}`"
-            append-icon="mdi-arrow-right"
-            variant="text"
-            size="x-large"
-          >
-            <div class="d-flex flex-column mt-n1 align-end">
-              <span class="text-button"> Next </span>
-              <span class="text-body-1 mt-n2"> {{ nextBlog.title }} </span>
-            </div>
-          </v-btn>
-        </v-col>
-      </v-row>
+      <v-col class="text-right">
+        <v-btn
+          v-if="nextBlog"
+          nuxt
+          :to="`/blog/${nextBlog.slug.current}`"
+          append-icon="mdi-arrow-right"
+          variant="text"
+          size="x-large"
+        >
+          <div class="d-flex flex-column mt-n1 align-end">
+            <span class="text-button"> Next </span>
+            <span class="text-body-1 mt-n2"> {{ nextBlog.title }} </span>
+          </div>
+        </v-btn>
+      </v-col>
+    </v-row>
 
-      <v-divider class="mt-4" />
-    </v-container>
-  </client-only>
+    <v-divider class="mt-4" />
+  </v-container>
 </template>
 
 <script setup lang="ts">
