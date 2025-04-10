2d2a10969f66b0ea52a513f0b26622ba0c3d4f18
Stuart Sewell <stuart@preoptima.com>
2025-04-10 17:15:48 +0100

    Refactor AppFooter component to simplify layout and remove unused elements

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,12 +1,24 @@
 <template>
-  <v-footer app color="teal-lighten-5" height="36">
+  <v-footer color="teal-lighten-5">
     <v-container fluid>
       <v-row>
-        <v-col cols="4" class="d-flex align-center justify-start">
+        <v-col
+          cols="12"
+          md="4"
+          class="d-flex align-center justify-center justify-md-start py-2"
+          order="3"
+          order-md="1"
+        >
           <span class="text-caption"> © 2025 junomidwives.co.uk </span>
         </v-col>
 
-        <v-col cols="4" class="d-flex align-center justify-center ga-8">
+        <v-col
+          cols="12"
+          md="4"
+          class="d-flex align-center justify-center justify-md-center ga-4 ga-md-8 py-2"
+          order="1"
+          order-md="2"
+        >
           <nuxt-link
             href="https://www.instagram.com/junomidwives/"
             target="_blank"
@@ -26,74 +38,27 @@
           </nuxt-link>
         </v-col>
 
-        <v-col cols="4" class="d-flex align-center justify-end ga-4">
-          <v-btn
-            nuxt
-            color="surface-variant"
-            variant="text"
+        <v-col
+          cols="12"
+          md="4"
+          class="d-flex align-center justify-center justify-md-end ga-2 ga-md-4 py-2"
+          order="2"
+          order-md="3"
+        >
+          <nuxt-link
             to="/privacy-policy"
-            class="text-caption"
+            class="text-caption text-surface-variant"
           >
             Privacy Policy
-          </v-btn>
-
-          <v-btn
-            nuxt
-            color="surface-variant"
-            variant="text"
+          </nuxt-link>
+          <nuxt-link
             to="/terms-conditions"
-            class="text-caption"
+            class="text-caption text-surface-variant"
           >
-            Teams and Conditions
-          </v-btn>
+            Terms and Conditions
+          </nuxt-link>
         </v-col>
       </v-row>
     </v-container>
-
-    <!-- <div class="d-flex align-center justify-space-between w-100">
-      <span class="text-caption"> © 2025 junomidwives.co.uk </span>
-
-      <span class="text-center d-flex align-center ga-6">
-        <nuxt-link
-          href="https://www.instagram.com/junomidwives/"
-          target="_blank"
-        >
-          <v-icon color="surface-variant" icon="mdi-instagram" />
-        </nuxt-link>
-
-        <nuxt-link to="/contact-us">
-          <v-icon color="surface-variant" icon="mdi-email" />
-        </nuxt-link>
-
-        <nuxt-link
-          href="https://www.facebook.com/profile.php?id=100093169811850"
-          target="_blank"
-        >
-          <v-icon color="surface-variant" icon="mdi-facebook" />
-        </nuxt-link>
-      </span>
-
-      <div class="d-flex align-center justify-end ga-1">
-        <v-btn
-          nuxt
-          color="surface-variant"
-          variant="text"
-          to="/privacy-policy"
-          class="text-caption text-decoration-underline text-surface-variant"
-        >
-          Privacy Policy
-        </v-btn>
-
-        <v-btn
-          nuxt
-          color="surface-variant"
-          variant="text"
-          to="/terms-conditions"
-          class="text-caption text-decoration-underline text-surface-variant"
-        >
-          Teams and Conditions
-        </v-btn>
-      </div>
-    </div> -->
   </v-footer>
 </template>
