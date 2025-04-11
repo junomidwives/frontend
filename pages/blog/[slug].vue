b1345a1bde5659328c485aeecdbff1d3a776615c
Stuart Sewell <stuart@preoptima.com>
2025-04-11 16:54:26 +0100

    Refactor navigation buttons in blog post component for improved layout and accessibility

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -17,45 +17,55 @@
       <SanityContent :blocks="blog.content?.body" :serializers="serializers" />
     </div>
 
-    <v-divider class="mt-16 mb-4" />
+    <v-divider class="mt-16" />
 
     <v-row>
-      <v-col>
-        <v-btn
-          v-if="previousBlog"
-          nuxt
-          :to="`/blog/${previousBlog.slug.current}`"
-          prepend-icon="mdi-arrow-left"
-          variant="text"
-          size="x-large"
-        >
-          <div class="d-flex flex-column mt-n1">
-            <span class="text-button"> Previous </span>
-            <span class="text-body-1 mt-n2">
-              {{ previousBlog.title }}
-            </span>
-          </div>
-        </v-btn>
+      <v-col cols="11" sm="6">
+        <v-hover>
+          <template #default="{ isHovering, props }">
+            <nuxt-link
+              v-if="previousBlog"
+              v-bind="props"
+              :to="`/blog/${previousBlog.slug.current}`"
+              class="d-flex align-center justify-start ga-4 text-decoration-none text-surface-variant py-1 py-md-4 h-100"
+              :class="{ 'bg-grey-lighten-5': isHovering }"
+            >
+              <v-icon>mdi-arrow-left</v-icon>
+              <div class="d-flex flex-column">
+                <span class="text-button font-weight-bold"> Previous </span>
+                <span class="text-body-2 text-md-body-1 mt-n2 text-wrap">
+                  {{ previousBlog.title }}
+                </span>
+              </div>
+            </nuxt-link>
+          </template>
+        </v-hover>
       </v-col>
 
-      <v-col class="text-right">
-        <v-btn
-          v-if="nextBlog"
-          nuxt
-          :to="`/blog/${nextBlog.slug.current}`"
-          append-icon="mdi-arrow-right"
-          variant="text"
-          size="x-large"
-        >
-          <div class="d-flex flex-column mt-n1 align-end">
-            <span class="text-button"> Next </span>
-            <span class="text-body-1 mt-n2"> {{ nextBlog.title }} </span>
-          </div>
-        </v-btn>
+      <v-col offset="1" offset-sm="0" cols="11" sm="6">
+        <v-hover>
+          <template #default="{ isHovering, props }">
+            <nuxt-link
+              v-bind="props"
+              v-if="nextBlog"
+              :to="`/blog/${nextBlog.slug.current}`"
+              class="d-flex align-center justify-end ga-4 text-decoration-none text-surface-variant py-1 py-md-4 h-100"
+              :class="{ 'bg-grey-lighten-5': isHovering }"
+            >
+              <div class="d-flex flex-column align-end">
+                <span class="text-button font-weight-bold"> Next </span>
+                <span class="text-body-2 text-md-body-1 mt-n2 text-wrap">
+                  {{ nextBlog.title }}
+                </span>
+              </div>
+              <v-icon>mdi-arrow-right</v-icon>
+            </nuxt-link>
+          </template>
+        </v-hover>
       </v-col>
     </v-row>
 
-    <v-divider class="mt-4" />
+    <v-divider />
   </v-container>
 </template>
 
