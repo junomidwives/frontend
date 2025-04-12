0550e3d382248060e8d5bbc0c3037a6020a82480
Stuart Sewell <stuart@preoptima.com>
2025-04-12 10:24:27 +0100

    feat: add contact us page with form for user inquiries

---
+++ pages/contact-us.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container max-width="600" class="mx-auto">
+    <v-form name="contact" netlify>
+      <v-text-field
+        v-model="name"
+        label="Name"
+        name="name"
+        type="text"
+        required
+      />
+      <v-text-field
+        v-model="email"
+        label="Email"
+        name="email"
+        type="email"
+        required
+      />
+      <v-text-field
+        v-model="message"
+        label="Message"
+        name="message"
+        type="text"
+        required
+      />
+      <v-btn type="submit" color="primary" class="mt-4">Submit</v-btn>
+    </v-form>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+const { setHeroText } = useHero();
+setHeroText("Contact Us");
+</script>
