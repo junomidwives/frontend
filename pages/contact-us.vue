9ecfc93e7c17aba2d142a2b53629717cb55b3061
Stuart Sewell <stuart@preoptima.com>
2025-04-12 10:34:56 +0100

    refactor: simplify v-text-field components in contact form for improved readability

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,27 +1,9 @@
 <template>
   <v-container max-width="600" class="mx-auto">
     <v-form name="contact" netlify>
-      <v-text-field
-        v-model="name"
-        label="Name"
-        name="name"
-        type="text"
-        required
-      />
-      <v-text-field
-        v-model="email"
-        label="Email"
-        name="email"
-        type="email"
-        required
-      />
-      <v-text-field
-        v-model="message"
-        label="Message"
-        name="message"
-        type="text"
-        required
-      />
+      <v-text-field label="Name" name="name" type="text" required />
+      <v-text-field label="Email" name="email" type="email" required />
+      <v-text-field label="Message" name="message" type="text" required />
       <v-btn type="submit" color="primary" class="mt-4">Submit</v-btn>
     </v-form>
   </v-container>
