47a39a09c04f762a9758a09289398e86d6700daf
Stuart Sewell <stuart@preoptima.com>
2025-04-13 11:20:39 +0100

    feat: add PDF viewer component and integrate with privacy policy and terms conditions pages

---
+++ components/PDFViewer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-for="page in pages" :key="page" class="d-flex justify-center">
+    <VuePDF :pdf="pdf" :page="page" scale="1.5" />
+  </div>
+</template>
+
+<script setup>
+import { VuePDF, usePDF } from "@tato30/vue-pdf";
+
+const props = defineProps({
+  pdf: String,
+});
+
+const { pdf, pages } = usePDF(props.pdf);
+</script>
