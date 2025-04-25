2f7e8877696117200cb1f9c0de8691be3ce01f55
Stuart Sewell <stuart@preoptima.com>
2025-04-25 09:45:43 +0100

    fix: add favicon and apple touch icon to app configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -53,4 +53,28 @@ export default defineNuxtConfig({
       );
     },
   },
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-icon-180x180.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+      ],
+    },
+  },
 });
