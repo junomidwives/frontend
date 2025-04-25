7beebdcec95b587477bb368f5d6d4d710a379de4
Stuart Sewell <stuart@preoptima.com>
2025-04-25 11:24:54 +0100

    fix: update description for improved SEO and clarity

--- app.vue
+++ app.vue
@@ -7,7 +7,8 @@
 <script setup lang="ts">
 useSeoMeta({
   title: "Juno Midwives",
-  description: "Independent Midwives in Kent",
+  description:
+    "Juno Midwives are NMC registered, independent midwives, with over 22 years combined experience and a passion for providing personalised midwifery care",
   ogTitle: "Juno Midwives",
   ogDescription: "Independent Midwives in Kent",
   ogImage: "https://junomidwives.co.uk/og-image.png",
