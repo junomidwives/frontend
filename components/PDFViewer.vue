030be7ed9ded75e941466fa4b5686c527dfdad06
Stuart Sewell <stuart@preoptima.com>
2025-08-02 18:13:50 +0100

    refactor: replace VuePDF component with custom canvas rendering for improved performance

--- components/PDFViewer.vue
+++ components/PDFViewer.vue
@@ -1,15 +1,66 @@
 <template>
-  <div v-for="page in pages" :key="page" class="d-flex justify-center">
-    <VuePDF :pdf="pdf" :page="page" scale="1.5" />
-  </div>
+  <canvas ref="canvasRef" style="border: 1px solid #ccc"></canvas>
+  <v-pagination
+    v-model="pageNo"
+    :length="totalPages"
+    density="compact"
+    size="small"
+  />
 </template>
 
-<script setup>
-import { VuePDF, usePDF } from "@tato30/vue-pdf";
+<script setup lang="ts">
+import { ref, onMounted, watch } from "vue";
 
 const props = defineProps({
-  pdf: String,
+  pdf: {
+    type: String,
+    required: true,
+  },
 });
 
-const { pdf, pages } = usePDF(props.pdf);
+const canvasRef = ref<HTMLCanvasElement | null>(null);
+const pageNo = ref<number>(1);
+const pdfjsLib = ref<any>(null);
+let pdf: any = null;
+const totalPages = ref(0);
+
+const loadPDF = async () => {
+  if (!props.pdf) return;
+
+  const pdfjs = await import("pdfjs-dist");
+  const pdfjsWorkerImport = await import("pdfjs-dist/build/pdf.worker?url");
+  pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorkerImport.default;
+  pdfjsLib.value = pdfjs;
+
+  const loadingTask = pdfjsLib.value.getDocument(props.pdf);
+  pdf = await loadingTask.promise;
+  totalPages.value = pdf.numPages;
+  await renderPage(pageNo.value);
+};
+
+async function renderPage(pageNum: number) {
+  const page = await pdf.getPage(pageNum);
+  const viewport = page.getViewport({ scale: 1.5 });
+  const canvas = canvasRef.value;
+  const context = canvas?.getContext("2d");
+
+  if (!canvas || !context) return;
+
+  canvas.height = viewport.height;
+  canvas.width = viewport.width;
+
+  await page.render({ canvasContext: context, canvas, viewport }).promise;
+}
+
+onMounted(async () => {
+  loadPDF();
+});
+
+watch(props, () => {
+  renderPage(pageNo.value);
+});
+
+watch(pageNo, (newPage) => {
+  renderPage(newPage);
+});
 </script>
