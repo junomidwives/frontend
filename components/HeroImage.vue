9ff0afdf83dbf7ae919cce2dd892a2f9befd2689
Stuart Sewell <stuart@preoptima.com>
2026-03-29 13:50:20 +0100

    fix: ensure single H1 per page by demoting heading levels in HeroImage, FAQ, and LogoGroup components

--- components/HeroImage.vue
+++ components/HeroImage.vue
@@ -7,12 +7,7 @@
       :max-height="400"
       class="hero-image"
     >
-      <v-sheet
-        width="100%"
-        height="100%"
-        color="transparent"
-        class="text-h5 text-sm-h3 text-md-h2 text-white"
-      >
+      <v-sheet width="100%" height="100%" color="transparent">
         <v-container fluid class="h-100 ps-4 ps-md-12 pb-0">
           <v-row class="h-100">
             <v-col
@@ -21,15 +16,21 @@
               cols="12"
               md="10"
             >
-              <p class="font-weight-bold">{{ heroText }}</p>
+              <h1
+                class="text-h5 text-sm-h3 text-md-h2 text-white font-weight-bold"
+              >
+                {{ heroText }}
+              </h1>
             </v-col>
 
             <v-col v-else class="d-flex flex-column justify-end pb-0 pb-md-4">
-              <p class="font-weight-bold">Natasha & Sophie</p>
-              <p class="font-weight-thin">
-                Independent Midwives<br />
-                in Kent
-              </p>
+              <h1 class="text-h5 text-sm-h3 text-md-h2 text-white">
+                <p class="font-weight-bold">Natasha & Sophie</p>
+                <p class="font-weight-thin">
+                  Independent Midwives<br />
+                  in Kent
+                </p>
+              </h1>
             </v-col>
           </v-row>
         </v-container>
